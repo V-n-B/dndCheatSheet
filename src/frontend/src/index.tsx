d5aeae352e40c43ebf388dc2c8c5a08772efed1d
voinik <victor_panteleev@hotmail.com>
2020-03-15 13:20:53 +0100

    Removed entry

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,17 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { polyfill } from 'es6-promise';
 import 'font-awesome/css/font-awesome.min.css';
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import { render } from 'react-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import { Entry } from './entry';
+import App from './components/App';
+import { Dashboard } from './components/home/Dashboard';
+import { Login } from './components/login/Login';
+import { Register } from './components/register/Register';
 import './scss/dnd-cheat-sheet.scss';
 
 polyfill();
 
+const browserHistory = createBrowserHistory();
+
 if (document.getElementById('app')) {
     render(
-        <Entry />,
+        <Router history={browserHistory}>
+            <App>
+                <Switch>
+                    <Route path="/" exact>
+                        <Dashboard />
+                    </Route>
+                    <Route path="/login" exact>
+                        <Login />
+                    </Route>
+                    <Route path="/register" exact>
+                        <Register />
+                    </Route>
+                </Switch>
+            </App>
+        </Router>,
         document.getElementById('app')
     );
 }
