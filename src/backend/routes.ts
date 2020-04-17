23a8e6854109430dc0e8454db19a256c5df587b6
voinik <victor_panteleev@hotmail.com>
2020-04-17 17:20:23 +0200

    Fixed logout error (#17)

--- src/backend/routes.ts
+++ src/backend/routes.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import path from 'path';
 import * as env from './contexts/env/envController';
 import * as user from './contexts/user/userController';
-import { checkToken } from './middleware/checkToken';
-import { checkUser } from './middleware/checkUser';
 import { noCaching } from './middleware/noCaching';
 import { handlePromisedRoute } from './utils/handlePromisedRoute';
 
@@ -14,7 +12,7 @@ export function routes(router: express.Router) {
     router.post('/api/users/verifyAccount', noCaching, handlePromisedRoute(user.verifyAccount));
     router.post('/api/users/login', noCaching, handlePromisedRoute(user.login));
     router.get('/api/users/session', noCaching, handlePromisedRoute(user.getCurrentSession));
-    router.delete('/api/users/logout', noCaching, checkUser, checkToken, handlePromisedRoute(user.logout));
+    router.delete('/api/users/logout', noCaching, handlePromisedRoute(user.logout));
 
     router.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/index.html'));
